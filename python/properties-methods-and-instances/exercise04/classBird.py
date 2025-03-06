3d1ae23b2262d44ea58dadce88e90edcb7eb3c83
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2025-03-06 18:21:43 -0400

    add python exercise04

---
+++ python/properties-methods-and-instances/exercise04/classBird.py
@@ -0,0 +1,5 @@
+class Bird:
+  # Empty constructor - If you want a totally empty object, you can leave the constructor empty and set attributes later.
+  # Empty constructor (pass) → Only if you want full manual control.
+  def __init__(self):
+    pass
---
+++ python/properties-methods-and-instances/exercise04/main.py
@@ -0,0 +1,11 @@
+from classBird import Bird
+
+bird1 = Bird()
+
+bird1.name = "Polly"
+bird1.age = 2
+bird1.species = "Parrot"
+
+print(bird1.name)
+print(bird1.age)
+print(bird1.species)
