55e08013eec6bc5ef45e60e1a17bc825273b8c49
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2025-03-26 15:43:58 -0400

    add java exercise03

---
+++ java/properties-methods-and-instances/exercise03/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Create two Student objects or instances of the Student class.
+        Student student1 = new Student("SpongeBob", 30, 3.40);
+        Student student2 = new Student("Patric", 27, 1.20);
+
+        // Use methods to show student information.
+        student1.showInfo();
+        System.out.println();
+        student2.showInfo();
+    }
+}
---
+++ java/properties-methods-and-instances/exercise03/src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+  // Attributes
+  String name;
+  int years;
+  double gpa;
+
+  // Constructor method that initializes the object's fields with the given arguments.
+  Student(String name, int years, double gpa) {
+    this.name = name;
+    this.years = years;
+    this.gpa = gpa;
+  }
+
+  // Method to show student information.
+  public void showInfo() {
+    System.out.println("Name: " + this.name);
+    System.out.println("Years: " + this.years);
+    System.out.println("Student gpa: " + this.gpa);
+  }
+}
